7e79a211a5be9d3b2f8672374ed1e76c2ef0493a
sathi5522 <sathi.nelavalli@gmail.com>
2020-04-10 15:29:51 +0530

    SA_NODEFER example using sigaction system call

---
+++ signals/sa_nodefer.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<signal.h>
+#include<string.h>
+#include <unistd.h>
+
+void sig_han(int signo,siginfo_t *obj,void *p)
+{
+	printf("the sighandler : %d\n",signo);
+	sleep(5);
+	printf("exit handler\n");
+}
+
+int main()
+{
+	struct sigaction act;
+	memset(&act, 0, sizeof(act));
+	act.sa_sigaction = sig_han;
+	act.sa_flags = SA_NODEFER;
+
+	if(sigaction(2, &act, NULL) == -1)
+		perror("sigaction");
+	printf("test \n");
+	getchar();
+	for(;;)
+		printf("the test section\n");
+
+	return 0;
+}
