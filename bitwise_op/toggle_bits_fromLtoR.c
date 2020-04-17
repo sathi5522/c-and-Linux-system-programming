dd0cdd725d98b8db0dde179c803526dad7935917
sathi5522 <sathi.nelavalli@gmail.com>
2020-04-17 12:01:21 +0530

    Toggle bits in the given range

---
+++ bitwise_op/toggle_bits_fromLtoR.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+  
+// function to toggle bits in the given range 
+unsigned int toggleBitsFromLToR(unsigned int n, 
+                                unsigned int l, unsigned int r) 
+{ 
+    // calculating a number 'num' having 'r' 
+    // number of bits and bits in the range l 
+    // to r are the only set bits 
+    int num = ((1 << r) - 1) ^ ((1 << (l - 1)) - 1); 
+  
+    // toggle bits in the range l to r in 'n' 
+    // and return the number 
+//Besides this, we can calculate num as: num=(1<<r)-l . 
+    return (n ^ num); 
+} 
+  
+// Driver program to test above 
+int main() 
+{ 
+    unsigned int n = 50; 
+    unsigned int l = 2, r = 5; 
+    printf("%u \n",toggleBitsFromLToR(n, l, r)); 
+    return 0; 
+} 
