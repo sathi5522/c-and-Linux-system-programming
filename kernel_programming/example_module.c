262abe8a2cf02a09a7f3310350e921d6ef4a6737
sathi5522 <sathi.nelavalli@gmail.com>
2020-04-27 11:50:47 +0530

    export symbol example

--- kernel_programming/example_module.c
+++ kernel_programming/example_module.c
@@ -2,10 +2,12 @@
 #include<linux/kernel.h>
 #include<linux/version.h>
 
+int val = 100;
+
 int my_init(void)
 {
-	printk(" example module inserted\n");
-	return 1;
+	printk(" example module inserted val: %d\n", val);
+	return 0;
 }
 
 void my_exit(void)
@@ -17,6 +19,7 @@ void my_exit(void)
 module_init(my_init);
 module_exit(my_exit);
 
+EXPORT_SYMBOL_GPL(val);
 MODULE_AUTHOR("satheesh");
 //MUDULE_DESCRIPTION("examplemodule");
 MODULE_LICENSE("GPL");
