2888d31cb762a2dfa11a3289a6464633c9bf99f2
sathi5522 <sathi.nelavalli@gmail.com>
2020-04-28 12:25:42 +0530

    char driver example for dynamic major number

--- drivers/char_drivers/char_drv_dynamic.c
+++ drivers/char_drivers/char_drv_dynamic.c
@@ -1,10 +1,10 @@
-/*Major and Minior number static*/
+/*Major and Minior number dynamic*/
 /*steps for creating char driver
-        1. create a device node
+        1. implementthe driver operations and register with vfs layer
+        2. insert the driverusing a kernel module
+                insmod char_drv_dynamic.ko
+        3. create a device node (see my_cdev major number in cat /proc/devices and use that major number when creating an device interface) 
                 mknod /dev/my_cdev c 300 0
-        2. implementthe driver operations and register with vfs layer
-        3. insert the driverusing a kernel module
-                insmod char_driver.ko
         4. write an application that initiates operations on our driver through common file api
 */
 
@@ -15,13 +15,10 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 
-#define MIN_NUM 0
-#define MAJOR_NUM 300
 #define CHAR_DEV_NAME "my_cdev"
 
 static dev_t mydev;
 struct cdev *mycdev;
-int count = 1, inuse = 0;
 
 int myopen(struct inode *inode, struct file *file)
 {
@@ -64,18 +61,28 @@ struct file_operations my_fops = {
 static int __init my_init(void)
 {
         int ret;
+	unsigned int minior = 0, count = 1;
 
-        mydev = MKDEV(MAJOR_NUM, MIN_NUM);
-        register_chrdev_region(mydev, count, CHAR_DEV_NAME);
+        if(alloc_chrdev_region(&mydev, minior, count, CHAR_DEV_NAME) < 0) {
+		printk(KERN_ERR"failed to reserve major/minior range\n");
+		return -1;
+	} 
 
 
 	mycdev = cdev_alloc();
+	if(mycdev == NULL) {
+		printk(KERN_ERR"cdv_alloc failed\n");
+		unregister_chrdev_region(mydev, count);
+		return -1;
+	}
         cdev_init(mycdev, &my_fops);
 
         ret = cdev_add(mycdev, mydev, count);
 
         if(ret < 0) {
                 printk("Error registering device driver\n");
+		cdev_del(mycdev);
+		unregister_chrdev_region(mydev, count);
                 return ret;
         }
         printk("char driver module loaded\n");
@@ -93,5 +100,5 @@ module_init(my_init);
 module_exit(my_exit);
 
 MODULE_AUTHOR("satheesh");
-MODULE_DESCRIPTION("MAJOR AND MINOR NUM STATIC");
+MODULE_DESCRIPTION("MAJOR AND MINOR NUM DYNAMIC");
 MODULE_LICENSE("GPL");
